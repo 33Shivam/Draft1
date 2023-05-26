c863cceacf46b76d79ac2903644c313373e17762
Your Name <sivamrockstar721@gmail.com>
2023-05-26 22:31:38 +0530

    footer done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import './pages/homepage.css';
 import './global.css';
 import './pages/homepage.js';
-import NavigationBar from './components/header/NavigationBar';
+import NavigationBar from './components/headerNfooter/NavigationBar.js';
 import Homepage from './pages/homepage.js';
-import Footer from './components/header/footer.js';
+import Footer from './components/headerNfooter/footer.js';
 
 function App() {
   return (
--- src/components/headerNfooter/footer.js
+++ src/components/headerNfooter/footer.js
@@ -7,10 +7,10 @@ class Footer extends React.Component {
             <div className="footer-c">
             <b className="contact-us">Contact Us</b>
             <div className="govt-of-india">© Govt. of India</div>
-            <img className="ellipse-icon" alt="" src="/ellipse.svg" />
-            <img className="ellipse-icon1" alt="" src="/ellipse1.svg" />
-            <img className="ellipse-icon2" alt="" src="/ellipse2.svg" />
-            <img className="ellipse-icon3" alt="" src="/ellipse3.svg" />
+            <img className="ellipse-icon" alt="" img src={require("../hnfAssests/social.png")} />
+            <img className="ellipse-icon1" alt="" img src={require("../hnfAssests/social1.png")} />
+            <img className="ellipse-icon2" alt="" img src={require("../hnfAssests/social2.png")} />
+            <img className="ellipse-icon3" alt="" img src={require("../hnfAssests/social4.png")} />
             <b className="help-desk">Help Desk</b>
             <b className="terms-and-conditions">Terms and conditions</b>
             <b className="website-policy">Website Policy</b>
